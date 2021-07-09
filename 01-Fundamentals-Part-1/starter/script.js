6875b4946cac29984cf0f6caff59a778b59b919b
Muneeb Anjum <tmuneebanjum@gmail.com>
2021-07-09 12:00:55 -0700

    Value and Variable

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,10 @@
 let js = "amazing";
-if (js === "amazing") alert("javascript is fun");
+// if (js === "amazing") alert("javascript is fun");
 
 console.log(40 + 10 + 40 + 12);
+
+console.log("Muneeb");
+console.log(23);
+
+let firstname = "Muneeb";
+console.log(firstname);
