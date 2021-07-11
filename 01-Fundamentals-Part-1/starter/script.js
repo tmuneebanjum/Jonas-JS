a330f61fca634a7e7f78449c18a03ef6ce00a4d2
Muneeb Anjum <tmuneebanjum@gmail.com>
2021-07-11 03:02:16 -0700

    data types and variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,10 +1,18 @@
-let js = "amazing";
-// if (js === "amazing") alert("javascript is fun");
+// let js = "amazing";
+// // if (js === "amazing") alert("javascript is fun");
 
-console.log(40 + 10 + 40 + 12);
+// console.log(40 + 10 + 40 + 12);
 
-console.log("Muneeb");
-console.log(23);
+// console.log("Muneeb");
+// console.log(23);
 
-let firstname = "Muneeb";
-console.log(firstname);
+// let firstname = "Muneeb";
+// console.log(firstname);
+// true;
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Muneeb");
