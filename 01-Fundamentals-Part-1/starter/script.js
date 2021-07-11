a89789967f2405b401ac645e6c505f7afc299b94
Muneeb Anjum <tmuneebanjum@gmail.com>
2021-07-11 03:32:05 -0700

    dynamic typing and error in null

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -9,10 +9,29 @@
 // let firstname = "Muneeb";
 // console.log(firstname);
 // true;
+
+//data types and variables
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
-console.log(typeof true);
+// console.log(typeof true);
 console.log(typeof javascriptIsFun);
-console.log(typeof 23);
-console.log(typeof "Muneeb");
+// console.log(typeof 23);
+// console.log(typeof "Muneeb");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+//dynamic typing
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+//bug in js null return object
+
+console.log(typeof null);
